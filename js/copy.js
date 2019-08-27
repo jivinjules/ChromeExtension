a7a9496602348887504f8fd81cc8455d6aa0de4b
TheRandomOneBTC <46252094+TheRandomOneBTC@users.noreply.github.com>
2019-08-27 08:13:12 -0500

    functions

--- js/copy.js
+++ js/copy.js
@@ -1,18 +1,12 @@
-// function connect() {
-//     console.log("HI")
-//     // var connect = document.getElementById('copy')
-//     const connectCode = "03a8355790b89f4d96963019eb9413b9a2c884691837ac976bacfe25a5212892d7@99.71.113.187:9735"
-//     connectCode.select();
-//     connect.setSelectionRange(0, 99999)
-//     document.execCommand('copy');
-//     alert(`Copied ${connectCode}`)
-// }
-
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log('HI')
     var copy = document.getElementById('copy')
     copy.addEventListener('click', function (event) {
         copyTextToClipboard('03a8355790b89f4d96963019eb9413b9a2c884691837ac976bacfe25a5212892d7@99.71.113.187:9735')
+    })
+    var close = document.getElementById('close')
+    close.addEventListener('click', function (event) {
+        window.close()
     })
 });
 
@@ -22,4 +16,6 @@ function copyTextToClipboard(text) {
             console.log('Lightning connection code copied')
         }
     )
+
+
 }
